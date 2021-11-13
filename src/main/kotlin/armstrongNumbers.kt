406a2b8af250a3eb25d6b8c9b1cf2d7f2e6e6869
Ma3tin <martin.mares@educanet.cz>
2021-11-13 16:48:31 +0100

    Armstrong Number2

--- src/main/kotlin/armstrongNumbers.kt
+++ src/main/kotlin/armstrongNumbers.kt
@@ -1,20 +1,20 @@
 import java.util.*
-import kotlin.math.*
 
 fun main() {
     val sc = Scanner(System.`in`)
     val input = sc.nextInt()
     for (i in 0..input) {
-        var rn = 0.0
-        for (j in i.toString()) {
-            val a = j.digitToInt().toDouble()
-            val b = i.toString().length.toDouble()
-            rn += Math.pow(a, b)
-            if (rn == i.toDouble()) {
-                println(i)
-                rn = 0.0
-            }
-            else if (rn > i.toDouble()) break
-        }
+        if (isArmstrongNumber(i)) println(i)
     }
+}
+
+fun isArmstrongNumber(inputNumber: Int):Boolean {
+    var rn = 0.0
+    for (j in inputNumber.toString()) {
+        val a = j.digitToInt().toDouble()
+        val b = inputNumber.toString().length.toDouble()
+        rn += Math.pow(a, b)
+        if (rn == inputNumber.toDouble()) return true
+    }
+    return false
 }
